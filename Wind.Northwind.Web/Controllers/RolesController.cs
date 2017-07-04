7f6d1fc4de4afee16fda94d5a673ab16cf72140b
Wilson Fernandez <wfernandez@wind.com.do>
2017-07-04 08:04:24 -0400

    creando roles

--- Wind.Northwind.Web/Controllers/RolesController.cs
+++ Wind.Northwind.Web/Controllers/RolesController.cs
@@ -1,18 +1,33 @@
-﻿using Abp.Authorization;
+﻿using Abp.Application.Services.Dto;
+using Abp.Authorization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Wind.Northwind.Permissions;
+using Wind.Northwind.Roles;
 
 namespace Wind.Northwind.Web.Controllers
 {
     [AbpAuthorize]
     public class RolesController : NorthwindControllerBase
     {
+        private readonly IRoleAppService _roleAppService;
+        private readonly IPermissionAppService _permissionAppservice;
+
+        public RolesController(IRoleAppService roleAppService, IPermissionAppService permissionAppservice)
+        {
+            _roleAppService = roleAppService;
+            _permissionAppservice = permissionAppservice;
+        }
+
         // GET: Roles
         public ActionResult Index()
         {
+            var permission = _permissionAppservice.GetListPermission()
+                                                  .Items
+                                                  .Select(p => new ComboboxItemDto(p.Name, ""));
             return View();
         }
     }
